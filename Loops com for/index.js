1bb4d2cb1f9a24195f46aaee971c1848e9868a14
Gustavo Prado <144186260+gustavoprado0@users.noreply.github.com>
2023-12-11 19:27:49 -0300

    Add files via upload

---
+++ Loops com for/index.js
@@ -0,0 +1,13 @@
+let spaceship = "Helmet"
+let newSpaceship = ""
+
+for(let i = 0; i < spaceship.length; i++) {
+    if(spaceship[i] == "e") {
+        newSpaceship += "i"
+    }else {
+        newSpaceship += spaceship[i]
+    }
+}
+
+console.log(newSpaceship)
+
